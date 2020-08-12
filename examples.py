5f65058a7d0f733722e8f5f22ecca021457ba7da
Vivek Saini <vivek.saini@mail.utoronto.ca>
2020-08-12 17:23:01 -0400

    yep
    deleted more unneccessary files

--- examples.py
+++
@@ -1,8 +0,0 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel
-from PyQt5.QtGui import QPixmap
-import pyqtgraph as pg 
-import numpy as np
-import pyqtgraph.examples
-
-pyqtgraph.examples.run()
